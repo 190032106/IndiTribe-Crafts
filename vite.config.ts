2fc0e12461decc8a2fda6cf129773e9446ca111e
Sambasivarao Ambati <ambatisambasivarao3@gmail.com>
2022-04-13 09:30:00 +0100

    Initial project setup

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import viteTsConfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [
+    viteTsConfigPaths(),
+    tanstackStart(),
+    viteReact(),
+    tailwindcss(),
+  ],
+});
